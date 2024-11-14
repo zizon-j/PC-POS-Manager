cf27467351265d796db2a5e402203508c3abf6ef
manuel3764 <kimkim3764@naver.com>
2024-11-14 14:33:58 +0900

    add InfoFrame.java fix Seat_UI.java

---
+++ src/UI/InfoFrame.java
@@ -0,0 +1,21 @@
+package UI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class InfoFrame extends JFrame {
+
+    public InfoFrame(){
+
+        JLabel name = new JLabel("이름: ");
+
+        add(name);
+
+        setSize(200, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new InfoFrame();
+    }
+}
