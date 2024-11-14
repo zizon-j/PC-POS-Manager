bd1edf24364e1588bb8e9b03d897e769676065e0
manuel3764 <kimkim3764@naver.com>
2024-11-14 14:44:51 +0900

    fix. Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import UI.MemberManagement_UI;
 import UI.Seat_UI;
 
 import javax.swing.*;
@@ -7,12 +8,13 @@ public class Main extends JFrame{
     JTabbedPane main = new JTabbedPane();
 
     Seat_UI seat_ui = new Seat_UI();
+    MemberManagement_UI mmu = new MemberManagement_UI();
 
     public Main(){
         super("PC방 POS");
         // 각 add 뒤에 자신이 만든 페이지 추가 , JPanel을 받아와야됨
         main.add("좌석관리", seat_ui);
-        main.add("회원관리",new JTextArea());
+        main.add("회원관리",mmu);
         main.add("운영매출", new JTextArea());
         main.add("상품관리",new JTextArea());
         main.add("로그분석", new JTextArea());
@@ -20,7 +22,7 @@ public class Main extends JFrame{
 
         add(main);
 
-        setSize(800, 600);
+        setSize(1600, 900);
         setLocationRelativeTo(null);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
