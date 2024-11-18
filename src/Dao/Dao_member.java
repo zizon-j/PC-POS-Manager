f11bf821f6499069a2aa4628ca55bdbd7eaf342a
kimki <kimkim3764@naver.com>
2024-11-19 01:47:52 +0900

    feat . 서버 접속 완료 feat. mysql_connector_j_8_2_0.xml

---
+++ src/Dao/Dao_member.java
@@ -0,0 +1,9 @@
+package Dao;
+
+public class Dao_member {
+    Db_member db_member = new Db_member();
+
+    public Db_member getDb_member() {
+        return db_member;
+    }
+}
---
+++ src/Dao/Db_member.java
@@ -0,0 +1,9 @@
+package Dao;
+
+import java.util.Date;
+
+public class Db_member {
+    int member_no, left_time;
+    String member_name, member_id, member_pwd, sex, phone, varchar;
+    Date birthday, reg_date;
+}
