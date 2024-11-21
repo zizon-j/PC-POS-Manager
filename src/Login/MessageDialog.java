a4fbacf10a6fa0e4831a7dea6206403708215e98
kimki <kimkim3764@naver.com>
2024-11-21 12:05:04 +0900

    feat. MessageDialog.java 이동  merge. ksh, jsc kjh, jsc merge

--- src/Login/MessageDialog.java
+++ src/Login/MessageDialog.java
@@ -1,4 +1,4 @@
-package UI;
+package Login;
 
 import java.awt.*;
 import javax.swing.*;
--- src/Login/RegisterFrame.java
+++ src/Login/RegisterFrame.java
@@ -95,15 +95,15 @@ class RegisterFrame extends JFrame implements ActionListener {
             String enteredId = idField.getText().trim();
 
             if (enteredId.isEmpty()) {
-                Login_MessageDialog md = new Login_MessageDialog(this, "ID 확인", true, "ID를 입력해주세요.");
+                MessageDialog md = new MessageDialog(this, "ID 확인", true, "ID를 입력해주세요.");
                 md.setLocationRelativeTo(this);
                 md.setVisible(true);
             } else if (enteredId.equals("2020081049")) { // Temporary ID for checking
-                Login_MessageDialog md = new Login_MessageDialog(this, "ID 확인", true, "알맞은 ID입니다.");
+                MessageDialog md = new MessageDialog(this, "ID 확인", true, "알맞은 ID입니다.");
                 md.setLocationRelativeTo(this);
                 md.setVisible(true);
             } else {
-                Login_MessageDialog md = new Login_MessageDialog(this, "ID 확인", true, "ID가 맞지 않습니다.");
+                MessageDialog md = new MessageDialog(this, "ID 확인", true, "ID가 맞지 않습니다.");
                 md.setLocationRelativeTo(this);
                 md.setVisible(true);
             }
